3661e6697a915322d2118d53d1490d151fd2f30f
fratzio <thomasmichaelfrazer@gmail.com>
2020-02-17 17:45:56 +1100

    Removed task related data and added tests for functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-/* Task 1.5 */
-
 // Create IIFE for app
 var pokemonRepository = (function() {
   // Pokedex repository
@@ -14,7 +12,6 @@ var pokemonRepository = (function() {
     { name: "snorlax", height: "120", type: ["rock", "water"] }
   ];
 
-  // SHOULD I PUT THIS FUNCTION INSIDE THE IIFE???
   // function for checking duplicates
   function checkDuplicate(newPokemon) {
     for (var i = 0; i < repository.length; i++) {
@@ -55,7 +52,9 @@ var pokemonRepository = (function() {
     if (result.length > 0) {
       document.write("Here is your Pokemon:" + "<br>");
       // return the complete object of the relative Pokemon
-      printArrayDetails(result);
+      Object.keys(result[0]).forEach(function(property) {
+        document.write("<br>" + property + ": " + result[0][property]);
+      });
       return "There's a match!";
     } else {
       return "There is no Pokemon with that name in the Pokedex";
@@ -73,29 +72,6 @@ var pokemonRepository = (function() {
   };
 })();
 
-/* 
-TESTING AREA
-
-test typeof checker
-pokemonRepository.add("Hitmonchan");
-
-test checkDuplicate method
-pokemonRepository.add({
-  name: "pikachu",
-  type: ["rock", "water"]
-});
-
-pokemonRepository.add({
-  name: "Blastoise",
-  type: ["rock", "water"]
-});
-
-// test searchPokemon
-console.log(pokemonRepository.search("snorlax"));
-console.log(pokemonRepository.search("Mewtwo"));
-*/
-
-document.write("<h1>Task 1.5</h1>");
 // refactored forEach loop
 Object.keys(pokemonRepository.getAll()).forEach(function(property) {
   if (pokemonRepository.getAll()[property].height > 100) {
@@ -117,83 +93,25 @@ Object.keys(pokemonRepository.getAll()).forEach(function(property) {
   }
 });
 
-/*
-Task 1.3
-*/
+/* 
+TESTING AREA
 
-var repository = [
-  { name: "pikachu", height: "78", type: ["thunder", "grass"] },
-  { name: "chameleon", height: "84", type: ["fire"] },
-  { name: "bulbasaur", height: "54", type: ["grass"] },
-  { name: "squirtle", height: "71", type: ["water"] },
-  { name: "geodude", height: "38", type: ["rock", "grass"] },
-  { name: "jigglypuff", height: "78", type: ["grass", "rock"] },
-  { name: "meowth", height: "57", type: ["rock", "fire"] },
-  { name: "snorlax", height: "120", type: ["rock", "water"] }
-];
 
-document.write("<h1>Task 1.3</h1>");
-// Creating a more dynamic function / take out hardcoded repo name
-function printArrayDetails(repositoryNumber) {
-  for (var i = 0; i < repositoryNumber.length; i++) {
-    if (repositoryNumber[i].height > 100) {
-      document.write(
-        repositoryNumber[i].name +
-          " (height: " +
-          repositoryNumber[i].height +
-          ') "Wow, that\'s big!"' +
-          "<br>"
-      );
-    } else {
-      document.write(
-        repositoryNumber[i].name +
-          " (height: " +
-          repositoryNumber[i].height +
-          ")" +
-          "<br>"
-      );
-    }
-  }
-}
-printArrayDetails(repository);
+test typeof checker
+pokemonRepository.add("Hitmonchan");
 
-/*
-Task 1.4
-*/
-document.write("<h1>Task 1.4</h1>");
-
-// using forEach method with an object
-repository.forEach(function(pokemon) {
-  if (pokemon.height > 100) {
-    document.write(
-      pokemon.name +
-        " (height: " +
-        pokemon.height +
-        ') "Wow, that\'s big!"' +
-        "<br>"
-    );
-  } else {
-    document.write(pokemon.name + " (height: " + pokemon.height + ")" + "<br>");
-  }
+test checkDuplicate method
+pokemonRepository.add({
+  name: "pikachu",
+  type: ["rock", "water"]
 });
 
-// forEach with the Object.keys method
-Object.keys(repository).forEach(function(property) {
-  if (repository[property].height > 100) {
-    document.write(
-      repository[property].name +
-        " (height: " +
-        repository[property].height +
-        ') "Wow, that\'s big!"' +
-        "<br>"
-    );
-  } else {
-    document.write(
-      repository[property].name +
-        " (height: " +
-        repository[property].height +
-        ")" +
-        "<br>"
-    );
-  }
+pokemonRepository.add({
+  name: "Blastoise",
+  type: ["rock", "water"]
 });
+
+// test searchPokemon
+console.log(pokemonRepository.search("snorlax"));
+console.log(pokemonRepository.search("Mewtwo"));
+*/
